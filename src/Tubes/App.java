99d87e739206a03ab64fe96238bbd536a7a6d58f
ASUS RYZEN <ASUS RYZEN@LAPTOP-9PMMSI13>
2019-12-03 15:08:07 +0700

    main

---
+++ src/Tubes/App.java
@@ -0,0 +1,39 @@
+package Tubes;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS RYZEN
+ */
+public class App {
+
+    public static void main(String[] args) {
+        String q1 = "What color are apples?\n"
+                + "(a)red/green\n(b)Orange\n(c)Magenta\n";
+
+        String q2 = "What color are bananas?\n"
+                + "(a)red/green\n(b)Yellow\n(c)Magenta\n";
+
+        //array and the answer
+        Question[] questions =  {
+            new Question(q1, "a"),
+            new Question(q2, "b")
+        };
+        takeTest(questions);
+
+    }
+
+    public static void takeTest(Question[] questions) {
+        int score = 0;
+        Scanner a = new Scanner(System.in);
+        for (int i = 0; i < questions.length; i++) {
+            System.out.println(questions[i].prompt);
+            String answer = a.nextLine();
+            if(answer.equals(questions[i].answer)){
+                score++;
+            }
+        }
+        System.out.println("You got "+ score+"/"+questions.length);
+    }
+}
---
+++ src/Tubes/Question.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tubes;
+
+/**
+ *
+ * @author ASUS RYZEN
+ */
+public class Question {
+
+    String prompt;
+    String answer;
+
+    public Question(String prompt, String answer) {
+        this.prompt = prompt;
+        this.answer = answer;
+    }
+
+}
